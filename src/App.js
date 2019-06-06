527d3538b52e5b321cf79a934d9d4bddf2a79d21
acui1 <51218056+acui1@users.noreply.github.com>
2019-06-06 16:57:42 -0400

    Practice commit for AC (git)

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import Dropdown from './Dropdown'
 import Results from './Results'
 
-const demographics = ['sampel1', 'sample2', 'sample3']
+const demographics = ['sample1', 'sample2', 'sample3']
 const education_and_employment = ['sample4', 'sample5', 'sample6']
 
 
