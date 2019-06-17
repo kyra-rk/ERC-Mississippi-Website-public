6860d82d82adde6abd0be52a1430611559d31fdd
winsomekaching <wtc2116@barnard.edu>
2019-06-17 14:59:06 -0400

    changed home link

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
     return (
       <div className="App">
         <nav class="navbar navbar-expand-lg navbar-light navbar-custom">
-          <a class="navbar-brand text-white" href="#">MS Women Count</a>
+          <a class="navbar-brand text-white" href="/home">MS Women Count</a>
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon navbar-inverse">
             </span>
