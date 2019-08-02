8a9d804666f24fa2e6dee5e89adb205f2874d23e
winsomekaching <wtc2116@barnard.edu>
2019-08-02 17:27:43 -0400

    added real quotes

--- src/data/selectedQuotes.js
+++ src/data/selectedQuotes.js
@@ -6,8 +6,8 @@ category: "Health",
 image: "person.png",
 audio: "MSaudio.mp3",
 quotes: {
-    quote1: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-    quote2: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    quote1: "Yes. So now I'm just looking forward to what's next. I'm excited. Just ready and waiting to just learn more and stuff.",
+    quote2: "As a matter of fact, I quit college at 19 and I got pregnant at 20.",
 }
 },
 {name: "Kandy Nickles", 
@@ -15,8 +15,7 @@ category: "Education",
 image: "person.png",
 audio: "MSaudio.mp3",
 quotes: {
-    quote1: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-    quote2: "Lorem ipsum dolor sit amet",
+    quote1: "I went to medical school in billing and coding and graduated. I went to a trade school and got business and office and then took a semester criminal justice and this school that I was going through paid for the semester at the University and Texas so I'm kind of a little bit certified in a little bit in everything.",
 }
 },
 {name: "Krista Augustine", 
@@ -24,7 +23,7 @@ category: "Education",
 image: "person.png",
 audio: "MSaudio.mp3",
 quotes: {
-    quote1: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    quote1: "And so now it's to get into the work field, but all those degrees and stuff that I have? Certificates, certification. It does not help me whatsoever here in Mississippi.",
 }
 },
 {name: "Meredith Dantzler", 
@@ -32,8 +31,8 @@ category: "Health",
 image: "person.png",
 audio: "MSaudio.mp3",
 quotes: {
-    quote1: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-    quote2: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    quote1: "I made it to 11th grade and I didn't complete high school. Maybe if my mom would have been a little strict on me, I could say say she wasn't...about me being a baby, she wasn't really strict on me because she believed in not having her kids have a hard life because of what she went through.",
+    quote2: "Alternative school. It's like a bad school because I had golds. I was in 11th grade. And she was the principal who made her own rules up.",
 }
 },
 {name: "Tanya Wills", 
@@ -41,7 +40,7 @@ category: "Education",
 image: "person.png",
 audio: "MSaudio.mp3",
 quotes: {
-    quote1: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    quote1: "So that school was to be honest, Ferriday High School was a very crooked school and now she got so many complaints that they fired her from being a principal. She went through a lot of different situations and wound up getting sick after all that she... she's stopped like 30 of us from graduating. So she's the reason a lot of us didn't graduate in that little small town.",
 }
 },
 {name: "Women in Construction Participant", 
@@ -49,8 +48,7 @@ category: "Health",
 image: "person.png",
 audio: "MSaudio.mp3",
 quotes: {
-    quote1: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-    quote2: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    quote1: "So that's why I stopped going to school and just proceeded with college. But that wasn't enough for me as well.",
 }
 },
 ]
