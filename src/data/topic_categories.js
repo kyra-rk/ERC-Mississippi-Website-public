db1345b32961bd57cf37d4f0a3208fbaffef6aec
Lalitha Madduri <lm3302@columbia.edu>
2019-12-06 12:51:23 -0500

    basic changes

--- src/data/topic_categories.js
+++ src/data/topic_categories.js
@@ -2,36 +2,36 @@
 
 const topic_categories = 
 [
-{name: "Interpersonal relationships",
-color: "#a7d6ce",
+{name: "Relationships",
+color: "#383f93",
 url: "relationships",
 },    
 {name: "Employment",
-color: "#d39e00",
+color: "#eabd24",
 url: "employment",
 },
-{name: "Structural issues",
-color: "#a38ddd",
+{name: "Structural",
+color: "#ce6a21",
 url: "structure",
 },
 {name: "Motherhood",
-color: "#dd7e6b",
+color: "#a866b3",
 url: "motherhood",
 },
 {name: "Care",
-color: "#a4c2f4",
+color: "#4886b3",
 url: "care",
 }, 
 {name: "Transportation",
-color: "#e09867",
+color: "#aa3a64",
 url: "transportation",
 },      
 {name: "Education",
-color: "#a7b2d6",
+color: "#933192",
 url: "education",
 },
-{name: "Financial situation",
-color: "#5bb635",
+{name: "Finances",
+color: "#3f98a3",
 url: "finance",
 },
 ]
@@ -72,31 +72,5 @@ url: "finance",
 // },
 // ]
 
-// const topic_categories = 
-// [{name: "Motherhood",
-// color: "#dc3545"
-// },
-// {name: "Care",
-// color: "green"
-// },       
-// {name: "Interpersonal relationships",
-// color: "brown"
-// },
-// {name: "Education",
-// color: "#17a2b8"
-// },
-// {name: "Financial situation",
-// color: "#d39e00"
-// },
-// {name: "Employment",
-// color: "#63053d"
-// },
-// {name: "Transportation",
-// color: "#e64700"
-// },
-// {name: "Understanding stuctural issues",
-// color: "#0f3766"
-// },
-// ]
 
 export default topic_categories
