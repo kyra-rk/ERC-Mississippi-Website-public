838c12320e0fd57a94843046bd52e9807bc2b5a3
winsomekaching <wtc2116@barnard.edu>
2019-12-05 22:35:41 -0500

    Removed last names and moved header to person data file

--- src/data/OHPersonData.js
+++ src/data/OHPersonData.js
@@ -3,8 +3,8 @@ const OHPersonData =
 {
 "id": 1,
 "header": [
-{"bio": "Claudine Logan is a mother of four and grandmother of two. Before Women in Construction, she worked as a supervisor for a construction firm in Jamaica, where she originally came from. Later, she lived in a shelter for women, where she found herself to be a mother figure to the other women living in the shelter. As someone who loves learning, Claudine enjoys getting hands-on with the construction training at WinC, and even hired a math teacher to improve at measurements. When Claudine graduates, she is looking forward to working at Ingalls Construction, where she has already secured a job at. She hopes to one day have her whole family under one roof, where her mother and son can be together with her and live in financial stability.",
-"realname": "Claudene Logan",
+{"bio": "Claudine is a mother of four and grandmother of two. Before Women in Construction, she worked as a supervisor for a construction firm in Jamaica, where she originally came from. Later, she lived in a shelter for women, where she found herself to be a mother figure to the other women living in the shelter. As someone who loves learning, Claudine enjoys getting hands-on with the construction training at WinC, and even hired a math teacher to improve at measurements. When Claudine graduates, she is looking forward to working at Ingalls Construction, where she has already secured a job at. She hopes to one day have her whole family under one roof, where her mother and son can be together with her and live in financial stability.",
+"realname": "Claudene",
 "initials": "CL",
 }],
 "interview": [
@@ -1273,8 +1273,8 @@ const OHPersonData =
 {
 "id": 2,
 "header": [
-    {"bio": "Kandy Nickles was born in California, and split her youth in California and Texas. She has had a diverse education background: medical school, trade school, a semester of criminal justice, and business technology. With those certifications, she went from working for private investigators, to selling cars to owning a cleaning business, but she had to leave the business behind when she left an abusive relationship. Kandy did not want her daughter to learn that abuse is condoned, therefore left for a better future for the both of them. Kandy made her way to Mississippi after getting engaged to a Mississippian, and together with her daughter, they relocated. Having had experience with male-dominated industries, Kandy hopes to create more opportunities for women by opening her own construction firm that is exclusively run by women.",
-    "realname": "Kandy Nickles",
+    {"bio": "Kandy was born in California, and split her youth in California and Texas. She has had a diverse education background: medical school, trade school, a semester of criminal justice, and business technology. With those certifications, she went from working for private investigators, to selling cars to owning a cleaning business, but she had to leave the business behind when she left an abusive relationship. Kandy did not want her daughter to learn that abuse is condoned, therefore left for a better future for the both of them. Kandy made her way to Mississippi after getting engaged to a Mississippian, and together with her daughter, they relocated. Having had experience with male-dominated industries, Kandy hopes to create more opportunities for women by opening her own construction firm that is exclusively run by women.",
+    "realname": "Kandy",
     "initials": "KN",
     }],
 "interview": [
--- src/data/selectedQuotes.js
+++ src/data/selectedQuotes.js
@@ -2,9 +2,9 @@
 
 const selectedQuotes = 
 [{name: "Person1",
-fullname: "Claudene Logan", 
+fullname: "Claudene", 
 category: "Health",
-initials: "CL",
+initials: "C",
 audio: "MSaudio.mp3",
 quotes: {
     quote1: "Yes. So now I'm just looking forward to what's next. I'm excited. Just ready and waiting to just learn more and stuff.",
@@ -12,27 +12,27 @@ quotes: {
 }
 },
 {name: "Person2",
-fullname: "Kandy Nickles", 
+fullname: "Kandy", 
 category: "Education",
-initials: "KN",
+initials: "K",
 audio: "MSaudio.mp3",
 quotes: {
     quote1: "I went to medical school in billing and coding and graduated. I went to a trade school and got business and office and then took a semester criminal justice and this school that I was going through paid for the semester at the University and Texas so I'm kind of a little bit certified in a little bit in everything.",
 }
 },
 {name: "Person3",
-fullname: "Krista Augustine", 
+fullname: "Krista", 
 category: "Education",
-initials: "KA",
+initials: "K",
 audio: "MSaudio.mp3",
 quotes: {
     quote1: "And so now it's to get into the work field, but all those degrees and stuff that I have? Certificates, certification. It does not help me whatsoever here in Mississippi.",
 }
 },
 {name: "Person4",
-fullname: "Meredith Dantzler", 
+fullname: "Meredith", 
 category: "Health",
-initials: "MD",
+initials: "M",
 audio: "MSaudio.mp3",
 quotes: {
     quote1: "I made it to 11th grade and I didn't complete high school. Maybe if my mom would have been a little strict on me, I could say say she wasn't...about me being a baby, she wasn't really strict on me because she believed in not having her kids have a hard life because of what she went through.",
@@ -40,9 +40,9 @@ quotes: {
 }
 },
 {name: "Person5",
-fullname: "Tanya Wills", 
+fullname: "Tanya", 
 category: "Education",
-initials: "TW",
+initials: "T",
 audio: "MSaudio.mp3",
 quotes: {
     quote1: "So that school was to be honest, Ferriday High School was a very crooked school and now she got so many complaints that they fired her from being a principal. She went through a lot of different situations and wound up getting sick after all that she... she's stopped like 30 of us from graduating. So she's the reason a lot of us didn't graduate in that little small town.",
