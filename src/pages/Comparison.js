16d93c2726ab3ac29744fa7c79e0a28b0658e868
Tabara Nosiba <tnosiba26@gmail.com>
2020-03-25 16:13:20 -0400

    Graph for second variable not showing

--- src/pages/Comparison.js
+++ src/pages/Comparison.js
@@ -40,24 +40,9 @@ class Comparison extends Component {
       genderselected: 'F',
       buttonselected: 'Women',
     }
-    let selectedbuttons = [{varname1: "", varabbreviation1: ""}, {varname2: "", varabbreviation2: ""}]
-    // this.createButtons = this.createButtons.bind(this)
+    this.selectedbuttons = {var1: {varname1: "", varabbreviation1: ""}, var2:{varname2: "", varabbreviation2: ""}}
     this.handleClick = this.handleClick.bind(this)
 }
-// createButtons(){
-//   //Maps categories to a new DropdownButton
-//   let buttons =  categories.map((obj, index) => (
-//           <DropdownButton
-//               title={obj.catname}
-//               variant={obj.variant.toLowerCase()}
-//               key={index}>
-//           {obj.variables.map((item, i) => (
-//                <Dropdown.Item eventKey={i} onClick={this.handleClick.bind(this,index, i)} key={i}>{item.name}</Dropdown.Item>
-//           ))}
-//           </DropdownButton>
-//   ))
-//   return buttons;
-// }
 
   // handleClick(event){
   //   console.log(event.target)
@@ -73,40 +58,78 @@ class Comparison extends Component {
   //   console.log(this.state.varselected)
   //   this.setState({varselected: event.target.value})
   // }
+  getabbreviation(matchingvar, demselected, genderselected){
+    var abbrev = "";
+    var genderabbrev = "";
+    var totalabbrev = "";
+    if (matchingvar.type === "P"){
+      abbrev+= "P_";
+    }
+    if (matchingvar.universe==="H"){
+      if (genderselected==="F"){
+        genderabbrev = "_FemaleNoHusband"
+      }
+      else if (genderselected==="M"){
+        genderabbrev = "_MaleNoWife"
+      }
+      else if (genderselected==="A"){
+        totalabbrev = "Total_"
+      }
+      abbrev= abbrev + totalabbrev + matchingvar.abbreviation + genderabbrev
+      if (demselected !== "A"){
+        abbrev += "_" + demselected;
+      }
+    }
+    else if (matchingvar.universe==="I"){
+      if (genderselected==="A"){
+        genderabbrev= "Total_"
+      }
+      if (genderselected==="F"){
+        genderabbrev = "Female_"
+      }
+      else if (genderselected==="M"){
+        genderabbrev = "Male_"
+      }
+      abbrev = abbrev+ genderabbrev + matchingvar.abbreviation
+      if (demselected !== "A"){
+        abbrev += "_" + demselected;
+      }
+    }
+    return abbrev;
+
+  }
 
   handleClick(index, i){
     //console.log(event.target.value)
     console.log(index, i)
     const matchingvar = categories[index].variables[i];
     console.log(matchingvar)
-    // var desc = matchingvar.description;
-    // var longerdesc = matchingvar.longdesc;
-    // console.log(desc)
     // const matchingvar = categories[].variables[event.target.name[2]]
-    // var demselected = this.state.demselected;
-    // var genderselected = this.state.genderselected;
-    // var buttonselected = this.state.buttonselected;
-    // console.log("This is the")
-    // console.log(matchingvar.name)
-    // var abbreviation = this.getabbreviation(matchingvar, demselected, genderselected);
-    // if(this.selectedbuttons[0].varname1 != "" && this.selectedbuttons[1].varname2 == ""){
-    //   this.selectedbuttons[1].varname2 = matchingvar.name
-    //   this.selectedbuttons[1].varabbreviation2 = abbreviation
-    // }
-    // else if(this.selectedbuttons[0].varname1 == "" && this.selectedbuttons[1].varname2 != ""){
-    //   this.selectedbuttons[0].varname1 = matchingvar.name
-    //   this.selectedbuttons[0].varabbreviation1 = abbreviation
-    // }
-    // else if(this.selectedbuttons[0].varname1 == matchingvar.name){
-    //   this.selectedbuttons[0].varname1 = ""
-    //   this.selectedbuttons[0].varabbreviation1 = ""
-    // }
-    // else if(this.selectedbuttons[1].varname2 == matchingvar.name){
-    //   this.selectedbuttons[1].varname2 = ""
-    //   this.selectedbuttons[1].varabbreviation2 = ""
-    // }
-    // this.setState({currentvar: true, varname1: this.selectedbuttons[0].varname1,  varname2: this.selectedbuttons[1].varname2, varabbreviation1: this.selectedbuttons[0].varabbreviation1, varabbreviation2: this.selectedbuttons[1].varabbreviation2,
-    //   race: matchingvar.race, gender: matchingvar.gender, racegender: matchingvar.racegender, demselected: demselected, genderselected: genderselected, buttonselected: buttonselected, vardesc: desc, varlongdesc: longerdesc})
+    var demselected = this.state.demselected;
+    var genderselected = this.state.genderselected;
+    var buttonselected = this.state.buttonselected;
+    console.log("This is the")
+    console.log(matchingvar.name)
+    console.log(this.selectedbuttons.var1.varname1)
+    var abbreviation = this.getabbreviation(matchingvar, demselected, genderselected);
+    if(this.selectedbuttons.var1.varname1 === ""){
+      this.selectedbuttons.var1.varname1 = matchingvar.name
+      this.selectedbuttons.var1.varabbreviation1 = abbreviation
+    }
+    else if(this.selectedbuttons.var2.varname2 === "" && this.selectedbuttons.var1.varname1 !== matchingvar.name){
+      this.selectedbuttons.var2.varname2 = matchingvar.name
+      this.selectedbuttons.var2.varabbreviation2 = abbreviation
+    }
+    else if(this.selectedbuttons.var1.varname1 === matchingvar.name){
+      this.selectedbuttons.var1.varname1 = ""
+      this.selectedbuttons.var1.varabbreviation1 = ""
+    }
+    else if(this.selectedbuttons.var2.varname2 === matchingvar.name){
+      this.selectedbuttons.var2.varname2 = ""
+      this.selectedbuttons.var2.varabbreviation2 = ""
+    }
+    this.setState({currentvar: true, varname1: this.selectedbuttons.var1.varname1,  varname2: this.selectedbuttons.var2.varname2, varabbreviation1: this.selectedbuttons.var1.varabbreviation1, varabbreviation2: this.selectedbuttons.var2.varabbreviation2,
+      race: matchingvar.race, gender: matchingvar.gender, racegender: matchingvar.racegender, demselected: demselected, genderselected: genderselected, buttonselected: buttonselected})
   }
 
   render(){
@@ -141,7 +164,7 @@ class Comparison extends Component {
 {cards}
                 </Row>
                
-                <ComparisonMap datainput = {this.state.dataset} variable ={"P_IBP_E_F"} variable2 = {"P_High_school_graduate_(includes_equivalency)_E_F"}varname = {"Poverty"} varname2 ={"High School Education"}/>
+                <ComparisonMap datainput = {this.state.dataset} variable ={this.state.varabbreviation1} variable2 = {this.state.varabbreviation2}varname = {this.state.varname1} varname2 ={this.state.varname2}/>
 
                 </Container>
     </div>
